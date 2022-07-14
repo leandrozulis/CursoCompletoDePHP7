b183a354cdab0ec8d3dcc0c2f13a466be4eb4365
leandrozulis <leandro_zulli@hotmail.com>
2022-07-14 12:08:15 -0300

    comentario, concatenação

---
+++ variaveis/exemplo-02.php
@@ -0,0 +1,19 @@
+<?php
+/*
+$anoNascimento = 1990;
+
+$nomeCompleto = "";*/
+
+//Na linha de baixo temos uma variável com número no nome
+$nome1 = "João";
+
+$sobrenome = "Rangel";
+
+$nomeCompleto = $nome1 . " " . $sobrenome;
+
+echo $nomeCompleto;
+
+
+
+
+?>
