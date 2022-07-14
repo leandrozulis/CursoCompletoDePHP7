bc461f0931a093f545036e47fd0f32908e8534e7
leandrozulis <leandro_zulli@hotmail.com>
2022-07-14 15:56:33 -0300

    Método GET/POST:cria uma sequência de consultas,  REMOTE_ADDR and SCRIPT_NAME

--- variaveis/exemplo-04.php
+++ variaveis/exemplo-04.php
@@ -5,7 +5,7 @@ $nome = (int)$_GET["a"];
 // var_dump($nome);
 
 $ip = $_SERVER["REMOTE_ADDR"]; // => Localiza o IP do usuário
-$ipp = $_SERVER["SCRIPT_NAME"]; // => Verifica por onde o usuário passou.
+$ipp = $_SERVER["SCRIPT_NAME"]; // => Verifica por onde o usuário passou..
 
 echo $ip;
 echo $ipp;
