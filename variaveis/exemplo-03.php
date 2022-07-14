1ad78db57f1e1365f55417efa654165e3c47095a
leandrozulis <leandro_zulli@hotmail.com>
2022-07-14 15:11:19 -0300

    tipos de variaveis, comum, compostas e especiais

---
+++ variaveis/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+//////////////////////////////////
+
+$frutas = array('Abacaxi', 'laranja', 'manga');
+
+// echo $frutas[2];
+
+$nascimento = new DateTime();
+
+// var_dump($nascimento);
+
+//////////////////////////////////
+
+$arquivo = fopen('exemplo-03.php', 'r');
+
+// var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
+
+?>
