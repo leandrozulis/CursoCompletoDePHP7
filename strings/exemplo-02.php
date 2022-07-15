e21ca3aac0a2cecab71bd8c2f6ad0b170e75b154
leandrozulis <leandro_zulli@hotmail.com>
2022-07-14 23:35:24 -0300

    Strings: strtoupper, strtolower, str_replace, ucwords, ucfirst, strpos, substr, strlen

---
+++ strings/exemplo-02.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = "leandro zulli silva";
+
+$nome = strtoupper($nome);
+echo $nome;
+echo "<br>";
+$nome = strtolower($nome);
+echo $nome;
+echo "<br>";
+$nome = ucwords($nome);
+echo $nome;
+echo "<br>";
+$nome = ucfirst($nome);
+echo $nome;
+
+
+
+?>
